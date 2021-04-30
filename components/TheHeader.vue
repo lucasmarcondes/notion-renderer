2c64ecae15eb3a678debb7d905813fba288fdf0b
lucasmarcondes <lucas@marcondes.me>
2021-04-30 02:32:06 -0700

    final deployment changes for test

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,9 +2,15 @@
 	<div>
 		<b-navbar type="dark" variant="dark">
 			<b-navbar-nav>
-				<b-nav-item href="/">Home</b-nav-item>
-				<b-nav-item href="/streaks">Streaks</b-nav-item>
-				<b-nav-item href="/about">About</b-nav-item>
+				<b-nav-item>
+					<nuxt-link class="nav-link" to="/" prefetch>Home</nuxt-link>
+				</b-nav-item>
+				<b-nav-item>
+					<nuxt-link class="nav-link" to="/streaks" prefetch>Streaks</nuxt-link>
+				</b-nav-item>
+				<b-nav-item>
+					<nuxt-link class="nav-link" to="/about" prefetch>About</nuxt-link>
+				</b-nav-item>
 			</b-navbar-nav>
 		</b-navbar>
 	</div>
@@ -14,4 +20,9 @@
 	export default {}
 </script>
 
-<style></style>
+<style>
+	.nav-link {
+		color: white;
+		font-weight: bold;
+	}
+</style>
